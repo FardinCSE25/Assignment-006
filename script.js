843bdac16080cb609910e66378bac4796d5bf0a7
Fardin Ahmed <fardin.cse25@gmail.com>
2025-09-07 02:13:16 +0600

    major changes

---
+++ script.js
@@ -0,0 +1,23 @@
+// getting categories
+const loadCategories = () =>{
+    fetch("https://openapi.programming-hero.com/api/categories")
+    .then ((res) => res.json())
+    .then ((data) => {
+      const categories = data.categories
+        showCategories(categories)
+    })
+     .catch((err) => {
+      console.log(err);
+    });
+}
+loadCategories()
+// show categories
+
+const showCategories = (categories) => {
+    categories.forEach(category => {
+        const showCategories = document.getElementById("showCategories")
+    showCategories.innerHTML += `<h1 id="${category.id}" class="pl-2 py-2 cursor-pointer rounded-md hover:bg-[#15803D] hover:text-white">${category.category_name}</h1>`
+    });
+        // console.log(category)
+    
+}
