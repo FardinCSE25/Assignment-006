00cb89155b5771ed4a214c15dc9f1a5f77c7002d
Fardin Ahmed <fardin.cse25@gmail.com>
2025-09-09 23:32:29 +0600

    final commi4

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Calling elements
 const showCategoriesContainer = document.getElementById("showCategories");
 const showPlants = document.getElementById("showAllPlants");
-const cartContainer = document.getElementById("cartContainer")
-const totalAmount = document.getElementById("totalAmount")
+const cartContainer = document.getElementById("cartContainer");
+const totalAmount = document.getElementById("totalAmount");
 
 let cart = [];
 
@@ -45,7 +45,7 @@ const showCategories = (categories) => {
 // getting plants by categories
 const loadPlantsByCategory = (id) => {
   // console.log(id);
-  manageLoading(true)
+  manageLoading(true);
   fetch(`https://openapi.programming-hero.com/api/category/${id}`)
     .then((res) => res.json())
     .then((data) => {
@@ -58,7 +58,6 @@ const loadPlantsByCategory = (id) => {
 };
 // show plants by category
 const showPlantByCategory = (plants) => {
-  
   showPlants.innerHTML = "";
   plants.forEach((plant) => {
     showPlants.innerHTML += `<div id='${plants.id}' class="plantDiv p-4 bg-white rounded-lg mb-3">
@@ -72,38 +71,40 @@ const showPlantByCategory = (plants) => {
               <button class="w-full text-center rounded-[999px] bg-[#15803D] text-white py-3">Add to Cart</button>
             </div>`;
   });
-  manageLoading(false)
+  manageLoading(false);
 };
 // load plant details for modal
 const loadPlantDetail = (id) => {
   fetch(`https://openapi.programming-hero.com/api/plant/${id}`)
-  .then((res) => res.json())
-  .then((data) => {
-    showPlantDetail(data);
-    console.log(data)
-  })
-}
+    .then((res) => res.json())
+    .then((data) => {
+      showPlantDetail(data);
+      console.log(data);
+    });
+};
 
-// load cart detail 
+// load cart detail
 showPlants.addEventListener("click", (e) => {
-  if(e.target.innerText === "Add to Cart"){
-    const name = e.target.parentNode.children[1].innerText
-    const id = e.target.parentNode.id
-    const price = e.target.parentNode
-    const newPrice = price.querySelector(".price").innerText
+  if (e.target.innerText === "Add to Cart") {
+    const name = e.target.parentNode.children[1].innerText;
+    const id = e.target.parentNode.id;
+    const price = e.target.parentNode;
+    const newPrice = price.querySelector(".price").innerText;
     // console.log({newPrice})
     cart.push({
-      name : name,
-      price : newPrice,
-      id : id
-    })
-    showCart(cart)
+      name: name,
+      price: newPrice,
+      id: id,
+    });
+    showCart(cart);
   }
-})
+});
 // show cart
 const showCart = (cart) => {
-  cartContainer.innerHTML = ""
-  cart.forEach(cartDetail => {
+  console.log(cart);
+  
+  cartContainer.innerHTML = "";
+  cart.forEach((cartDetail) => {
     cartContainer.innerHTML += `<div class="bg-[#F0FDF4] px-3 py-2 mb-1 flex justify-between items-center rounded-lg">
                 <div>
                   <h1 class="mb-1 text-sm font-semibold">${cartDetail.name}</h1>
@@ -112,39 +113,37 @@ const showCart = (cart) => {
                 <div onclick="deleteCart(${cartDetail.id})">
                   <i  class="text-red-600 fa-solid fa-xmark"></i>
                 </div>
-              </div>`
-              let total = totalAmount.innerText
-            console.log(total)
-            console.log(cartDetail.price)
-      finalAmount = parseInt(total + cartDetail.price)
-      // console.log(finalAmount)
-      totalAmount.innerText = finalAmount
-            
-  })
-  
-}
+              </div>`;
+
+    let total = parseInt(totalAmount.innerText);
+    console.log(cartDetail.price);
+    
+    finalAmount = total + Number(cartDetail.price);
+    totalAmount.innerText = parseInt(finalAmount);
+
+  });
+};
 // delete cart
 const deleteCart = (id) => {
   // console.log(id)
-  const filteredCarts = cart.filter(cartDetail => cartDetail.id != id)
-  cart = filteredCarts
-  showCart(cart)
-}
-
+  const filteredCarts = cart.filter((cartDetail) => cartDetail.id != id);
+  cart = filteredCarts;
+  showCart(cart);
+};
 
 const showPlantDetail = (detail) => {
   // console.log(detail);
-  
-  const detailContainer = document.getElementById("detailContainer")
+
+  const detailContainer = document.getElementById("detailContainer");
   detailContainer.innerHTML = `   <div>
         <h2 class="text-2xl font-semibold mb-5">${detail.plants.name}</h2>
         <img class="rounded-2xl h-[300px] w-full mb-3" src="${detail.plants.image}" alt="${detail.plants.name}">
         <h2 class="font-semibold  mb-3">Category : <span class="font-light">${detail.plants.category}</span></h2>
         <h2 class="font-semibold  mb-3">Price : <span class="font-light">  ৳${detail.plants.price}</span></h2>
         <p class="font-semibold">Description : <span class="font-light">${detail.plants.description}</span></p>
-      </div>`
-  document.getElementById("plantDetail").showModal()
-}
+      </div>`;
+  document.getElementById("plantDetail").showModal();
+};
 
 // const showModal = (plantName) => {
 // showPlants.addEventListener("click", (e) => {
@@ -154,15 +153,14 @@ const showPlantDetail = (detail) => {
 
 // manage loading
 const manageLoading = (status) => {
-  if(status == true){
-    document.getElementById("loading").classList.remove("hidden")
-    document.getElementById("showAllPlants").classList.add("invisible")
+  if (status == true) {
+    document.getElementById("loading").classList.remove("hidden");
+    document.getElementById("showAllPlants").classList.add("invisible");
+  } else {
+    document.getElementById("showAllPlants").classList.remove("invisible");
+    document.getElementById("loading").classList.add("hidden");
   }
-  else {
-    document.getElementById("showAllPlants").classList.remove("invisible")
-    document.getElementById("loading").classList.add("hidden")
-  }
-}
+};
 
 // getting all plants
 const loadAllPlants = () => {
@@ -179,7 +177,7 @@ const loadAllPlants = () => {
 loadAllPlants();
 // show all plants
 const showAllPlants = (allPlants) => {
-  allPlants.innerHTML = ""
+  allPlants.innerHTML = "";
   allPlants.forEach((plants) => {
     // console.log(plants)
     const showAllPlants = document.getElementById("showAllPlants");
@@ -193,14 +191,13 @@ const showAllPlants = (allPlants) => {
               </div>
               <button class="w-full text-center rounded-[999px] bg-[#15803D] text-white py-3">Add to Cart</button>
             </div>`;
-  //            let total = totalAmount.innerText
-  //           console.log(total)
-  //           console.log(cartDetail.price)
-  //     finalAmount = parseInt(total + cartDetail.price)
-  //     // console.log(finalAmount)
-  //     totalAmount.innerText = finalAmount
+    //        let total = totalAmount.innerText
+    //       console.log(total)
+    //       console.log(cartDetail.price)
+    // finalAmount = parseInt(total + plants.price)
+    // // console.log(finalAmount)
+    // totalAmount.innerText = finalAmount
 
-  // console.log(category)
+    // console.log(category)
   });
-  
 };
